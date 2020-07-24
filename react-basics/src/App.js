e786f47a06f447ff17b07a4423ccaa907e8fa729
Deepak Singla <thejavascriptways@gmail.com>
2020-07-24 02:04:45 -0500

    functional component

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>First React App!</h1>
+        <h1>First React Application!</h1>
+        <Person name ="Messi" age ="33" />
+        <Person name ="Ronaldo" age ="35"> I am a soccer player!</Person>
+        <Person name ="Nothing" age ="99">I am Nothing</Person>
       </div>
     );
+    //return React.createElement('div', {className:'App'},React.createElement('h1',null,'React works!'));
   }
 }
 
