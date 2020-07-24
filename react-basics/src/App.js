318dffca3d50e89a949bc09562d2c3def537187f
Deepak Singla <thejavascriptways@gmail.com>
2020-07-24 02:59:19 -0500

    Event handler, state and setState

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -9,14 +9,26 @@ class App extends Component {
     persons : [
       { name : 'Messi', age: 30},
       { name : 'Ronaldo', age: 33},
-      { name : 'Mpappe', age: 26},
+      { name : 'Mpappe', age: 26}
     ]
+  };
+
+  switchNameHandler = () => {
+    this.setState(
+      {
+        persons:[
+          { name : 'Leo Messi', age: 30},
+          { name : ' Chris Ronaldo', age: 33},
+          { name : 'Mpappe', age: 28}
+        ] 
+      })
   };
 
   render() {
     return (
       <div className="App">
         <h1>First React Application!</h1>
+        <button onClick={this.switchNameHandler}>Switch</button>
         <Person name ={this.state.persons[0].name} age ={this.state.persons[0].age} />
         <Person name ={this.state.persons[1].name} age ={this.state.persons[1].age}> I am a soccer player!</Person>
         <Person name ={this.state.persons[2].name} age ={this.state.persons[2].age}>I am {this.state.persons[2].name}</Person>
