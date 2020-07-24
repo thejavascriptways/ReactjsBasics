6952b7f7e8b48bdc3ee2ca6e706bdd38de91f416
Deepak Singla <thejavascriptways@gmail.com>
2020-07-24 02:15:38 -0500

    Component State

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -4,13 +4,22 @@ import Person from './Person/Person';
 
 
 class App extends Component {
+
+  state = {
+    persons : [
+      { name : 'Messi', age: 30},
+      { name : 'Ronaldo', age: 33},
+      { name : 'Mpappe', age: 26},
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>First React Application!</h1>
-        <Person name ="Messi" age ="33" />
-        <Person name ="Ronaldo" age ="35"> I am a soccer player!</Person>
-        <Person name ="Nothing" age ="99">I am Nothing</Person>
+        <Person name ={this.state.persons[0].name} age ={this.state.persons[0].age} />
+        <Person name ={this.state.persons[1].name} age ={this.state.persons[1].age}> I am a soccer player!</Person>
+        <Person name ={this.state.persons[2].name} age ={this.state.persons[2].age}>I am {this.state.persons[2].name}</Person>
       </div>
     );
     //return React.createElement('div', {className:'App'},React.createElement('h1',null,'React works!'));
