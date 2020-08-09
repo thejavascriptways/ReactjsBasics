4d8ebb858b7f2485da239a3deb746ab2b269ca83
Deepak Singla <thejavascriptways@gmail.com>
2020-08-09 01:50:56 -0500

    LifeCycle Hooks for Updating Component

--- react-basics/src/components/Persons/Person/Person.js
+++ react-basics/src/components/Persons/Person/Person.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classPerson from './Person.css';
 
 
 
-const person = (props) =>{
+class Person extends Component{
 
-console.log('Person.js render');
+    static getDerivedStateFromProps=(props, state) =>{
+        console.log('Person.js getDerivedStateFromProps');
+        console.log(props);
+        console.log(state);
+        return state;
+      }
+    
+      shouldComponentUpdate = (nextProps, nextState) => {
+        console.log('Person.js shouldComponentUpdate');
+        console.log(nextProps);
+        console.log(nextState);
+        return true;
+      }
+    
+      getSnapshotBeforeUpdate = (prevProps, prevState) => {
+        console.log('Person.js getSnapshotBeforeUpdate');
+        console.log(prevProps);
+        console.log(prevState);
+        return true;
+      }
+    
+      componentDidUpdate = () =>{
+        console.log('Person.js componentDidUpdate');
+      }
 
-return (
-    <div className={classPerson.Person}>
-        <p onClick ={props.click}>I am {props.name} and my age is {props.age} </p>
-        <p>{props.children}</p>
-        <input type ="text" onChange={props.changed} value ={props.name}/>
-    </div>
-    )
-};
-
-export default person;
+    render(){
+        console.log('Person.js render');
+        return (
+            <div className={classPerson.Person}>
+                <p onClick ={this.props.click}>I am {this.props.name} and my age is {this.props.age} </p>
+                <p>{this.props.children}</p>
+                <input type ="text" onChange={this.props.changed} value ={this.props.name}/>
+            </div>
+            )
+        };
+    }
+    
+export default Person;
--- react-basics/src/components/Persons/Persons.js
+++ react-basics/src/components/Persons/Persons.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
-  console.log('Persons.js render');
-  return props.persons.map((person, index)=> {
-    return <Person
-      click = {() => props.clicked(index)}
-      changed = {(event) => props.changed(event,person.id)}
-      name ={person.name} 
-      age = {person.age}
-      key = {person.id} />    
-  });
+class Persons extends Component {
+
+  static getDerivedStateFromProps=(props, state) =>{
+    console.log('Persons.js getDerivedStateFromProps');
+    console.log(props);
+    console.log(state);
+    return state;
+  }
+
+  shouldComponentUpdate = (nextProps, nextState) => {
+    console.log('Persons.js shouldComponentUpdate');
+    console.log(nextProps);
+    console.log(nextState);
+    return true;
+  }
+
+  getSnapshotBeforeUpdate = (prevProps, prevState) => {
+    console.log('Persons.js getSnapshotBeforeUpdate');
+    console.log(prevProps);
+    console.log(prevState);
+    return true;
+  }
+
+  componentDidUpdate = () =>{
+    console.log('Persons.js componentDidUpdate');
+  }
+
+  render(){
+    console.log('Persons.js render');
+    return this.props.persons.map((person, index)=> {
+      return <Person
+        click = {() => this.props.clicked(index)}
+        changed = {(event) => this.props.changed(event,person.id)}
+        name ={person.name} 
+        age = {person.age}
+        key = {person.id} />    
+    });
+    }
+
 }
 
-  export default persons;
+export default Persons;
